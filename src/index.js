5197dd66274d3ce05893fad773424a25ebeb8ffa
chanyikchong <1642508849@qq.com>
2024-09-20 13:25:05 +0100

    [UDPATE]

--- src/index.js
+++ src/index.js
@@ -14,7 +14,7 @@ const root = ReactDOM.createRoot(document.getElementById('root'));
 
 root.render(
     <React.StrictMode>
-        <Router basename="/personal">
+        <Router basename="/tax-calculator">
             {/* Global Header */}
             <Header />
             {/* Define your routes */}
